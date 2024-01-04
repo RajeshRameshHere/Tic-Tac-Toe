b62ce29b010b22c255d9aff5619a14da67f4ed8a
Rajuthedevil <rajurajesh.aero.007@gmail.com>
2024-01-04 22:04:29 +0530

    create tic-tac-toe

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import TicTacToe from './components/Tic-tac-toe/TicTacToe';
+function App() {
+  return (
+    <div>
+      <TicTacToe/>
+    </div>
+  );
+}
+
+export default App;
